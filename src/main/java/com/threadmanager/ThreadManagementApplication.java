f762b4e0d23856526dddc802daa15dc118591fc8
zulalkarin <zulalkarin@hotmail.com>
2024-12-07 17:37:48 +0300

    Add ThreadManagementApplication class

---
+++ src/main/java/com/threadmanager/ThreadManagementApplication.java
@@ -0,0 +1,13 @@
+package com.threadmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadManagementApplication.class, args);
+    }
+
+} 
