b5aa44d703249ab56182479a870d2dec89e3b2fe
zulalkarin <zulalkarin@hotmail.com>
2024-12-07 17:39:08 +0300

    Refactor main method in TestThreadsApplication

--- src/test/java/thread/example/threads/TestThreadsApplication.java
+++ src/test/java/thread/example/threads/TestThreadsApplication.java
@@ -1,11 +1,13 @@
 package thread.example.threads;
 
+import com.threadmanager.ThreadManagementApplication;
+
 import org.springframework.boot.SpringApplication;
 
 public class TestThreadsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(ThreadsApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(ThreadManagementApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
